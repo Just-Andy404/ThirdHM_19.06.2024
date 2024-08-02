e4411e4ce2d6d8e12861543a966c6371a27641ab
Andy <andyivanov2007@gmail.com>
2024-08-02 20:45:26 +0300

    Add project files.

---
+++ ConsoleApp3/Performance.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Performance : IDisposable
+    {
+        public string Name { get; set; }
+        public string Theatre { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+        public List<string> Actors { get; set; }
+
+        public Performance(string name, string theatre, string genre, int duration, List<string> actors)
+        {
+            Name = name;
+            Theatre = theatre;
+            Genre = genre;
+            Duration = duration;
+            Actors = actors;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine($"Название спектакля: {Name}");
+            Console.WriteLine($"Театр: {Theatre}");
+            Console.WriteLine($"Жанр: {Genre}");
+            Console.WriteLine($"Длительность: {Duration}");
+            Console.WriteLine("Актеры:");
+            foreach (var actor in Actors)
+            {
+                Console.WriteLine($"{actor}");
+            }
+
+            return "";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Спектакль закончился с помощью Dispose");
+        }
+
+        ~Performance()
+        {
+            Console.WriteLine("Спектакль закончился");
+        }
+    }
+}
+
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Добавьте к первому заданию реализацию интерфейса IDisposable.
+Добавьте ко второй задаче реализацию деструктора. Напишите
+код для тестирования новых возможностей
+ */
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> actors = new List<string> { "Актер 1", "Актер 2", "Актер 3" };
+            Performance performance1 = new Performance("Ромео и Джульетта", "Национальный театр", "Драма", 120, actors);
+
+            Console.WriteLine(performance1);
+            performance1.Dispose();
+
+        }
+    }
+}
